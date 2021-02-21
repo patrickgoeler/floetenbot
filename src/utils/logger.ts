b562f8b9270febf4c200ee8200a8c4464749b21b
Patrick <patrickvongoeler@gmail.com>
2021-02-21 18:18:14 +0100

    logging?

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,8 +1,14 @@
-import winston from "winston"
+import winston, { format } from "winston"
+
+const { combine, timestamp: WinstomTimestamp, label: WinstonLabel, printf } = format
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`
+})
 
 const logger = winston.createLogger({
   transports: [new winston.transports.Console(), new winston.transports.File({ filename: "log/log" })],
-  format: winston.format.printf((log) => `[${log.level.toUpperCase()}] - ${log.message}`),
+  format: combine(WinstonLabel({ label: "right meow!" }), WinstomTimestamp(), myFormat),
 })
 
 export default logger
